70d2e1ea06dedb54c48d4861733d79a24523a6c2
Tamer Alamiri <tameralamiri@gmail.com>
2024-07-09 08:58:54 -0400

    add a default file

--- rusty-journal/src/main.rs
+++ rusty-journal/src/main.rs
@@ -1,15 +1,25 @@
 use structopt::StructOpt;
+use std::path::PathBuf;
 mod cli;
 mod tasks;
 
 use cli::{Action::*, CommandLineArgs};
 use tasks::Task;
+
+fn find_default_journal_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".rusty-journal.json");
+        path
+    })
+}
 fn main() {
     // Get the comand-line arguments.
     let CommandLineArgs { action, journal_file } = CommandLineArgs::from_args();
     
     // Unpack the journal file path.
-    let journal_file = journal_file.expect("Failed to find journal file");
+    let journal_file = journal_file
+        .or_else(find_default_journal_file)
+        .expect("Failed to find journal file.");
 
     // Perform the action.
     match action {
