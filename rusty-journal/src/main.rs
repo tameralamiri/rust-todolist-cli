0bf13fa601b628a73eff96998a55e035bb08a2f5
Tamer Alamiri <tameralamiri@gmail.com>
2024-07-09 09:10:59 -0400

    use anyhow for user friendly errors

--- rusty-journal/src/main.rs
+++ rusty-journal/src/main.rs
@@ -1,5 +1,6 @@
-use structopt::StructOpt;
+use anyhow::{anyhow, Ok};
 use std::path::PathBuf;
+use structopt::StructOpt;
 mod cli;
 mod tasks;
 
@@ -12,27 +13,26 @@ fn find_default_journal_file() -> Option<PathBuf> {
         path
     })
 }
-fn main() {
+fn main() -> anyhow::Result<()> {
     // Get the comand-line arguments.
-    let CommandLineArgs { action, journal_file } = CommandLineArgs::from_args();
-    
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
     // Unpack the journal file path.
     let journal_file = journal_file
         .or_else(find_default_journal_file)
-        .expect("Failed to find journal file.");
+        .ok_or(anyhow!("Failed to find a journal file"))?;
 
     // Perform the action.
     match action {
         Add { task } => {
             let task = Task::new(task);
-            tasks::add_task(journal_file, task).expect("Failed to add task");
+            tasks::add_task(journal_file, task)
         }
-        Done { position } => {
-            tasks::complete_task(journal_file, position).expect("Failed to complete task");
-        }
-        List => {
-            tasks::list_tasks(journal_file).expect("Failed to list tasks");
-        }
-        
-    }
+        Done { position } => tasks::complete_task(journal_file, position),
+        List => tasks::list_tasks(journal_file),
+    }?;
+    Ok(())
 }
